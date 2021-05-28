f67cc50bd6504b03dccb11d6b948655bad0d5096
Matias Aleksandrowicz <31048494+matiasalek@users.noreply.github.com>
2021-05-28 17:58:49 -0300

    Update hombresVsMujeresMuertos.py

--- PythonScripts/hombresVsMujeresMuertos.py
+++ PythonScripts/hombresVsMujeresMuertos.py
@@ -7,10 +7,20 @@ covid_csv = pd.read_csv('Covid19CasosCorregido.csv')
 # extraigo los muertos hombres en la variable muertos_hombres y lo mismo para las mujeres en la variable muertos_mujeres
 muertos_hombres = covid_csv.loc[(covid_csv['sexo'] == 'M') & (covid_csv['fallecido'] == 'SI'), 'sexo']
 muertos_mujeres = covid_csv.loc[(covid_csv['sexo'] == 'F') & (covid_csv['fallecido'] == 'SI'), 'sexo']
+muertos_total = 73067
+
+muertos_hombres_list = muertos_hombres.tolist()
+muertos_mujeres_list = muertos_mujeres.tolist()
+
+muertos_hombres_total = len(muertos_hombres_list)
+muertos_mujeres_total = len(muertos_mujeres_list)
+
+muertos_hombres_chart = muertos_hombres_total * 100 / muertos_total
+muertos_mujeres_chart = muertos_mujeres_total * 100 / muertos_total
 
 # lo que va a mostrar el grafico Pie Chart
 labels = ['Mujeres', 'Hombres']
-sizes = [42.01, 57.99]
+sizes = [muertos_mujeres_chart, muertos_hombres_chart]
 
 # creo el grafico
 fig1, ax1 = plt.subplots()
